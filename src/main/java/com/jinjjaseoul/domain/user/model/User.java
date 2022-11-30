614e4272a2752579ccdca0a64eaa39495206d3f5
yunhozz <qkrdbsgh1121@naver.com>
2022-11-30 12:42:04 +0900

    feat: 회원 관련 모델 추가

---
+++ src/main/java/com/jinjjaseoul/domain/user/model/User.java
@@ -0,0 +1,33 @@
+package com.jinjjaseoul.domain.user.model;
+
+import com.jinjjaseoul.common.enums.Role;
+import com.jinjjaseoul.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Role role; // ROLE_ADMIN, ROLE_USER
+}
