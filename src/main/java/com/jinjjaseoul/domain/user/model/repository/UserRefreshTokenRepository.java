fd8f7348c872f0c8313e4025a7f589935b5b0f9a
yunhozz <qkrdbsgh1121@naver.com>
2022-12-02 17:39:55 +0900

    feat: 유저 재발급 토큰 관련 모델 생성

---
+++ src/main/java/com/jinjjaseoul/domain/user/model/repository/UserRefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package com.jinjjaseoul.domain.user.model.repository;
+
+import com.jinjjaseoul.domain.user.model.entity.UserRefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRefreshTokenRepository extends JpaRepository<UserRefreshToken, Long> {
+
+    Optional<UserRefreshToken> findByUserId(Long userId);
+}
