a66906e5cd4806b6606b2d52706ec9084e42bed6
yunhozz <qkrdbsgh1121@naver.com>
2022-12-02 19:25:51 +0900

    feat: 유저 관련 dto, 컨버터 생성

---
+++ src/main/java/com/jinjjaseoul/domain/user/dto/UserRequestDto.java
@@ -0,0 +1,22 @@
+package com.jinjjaseoul.domain.user.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@NoArgsConstructor
+public class UserRequestDto {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String name;
+}
---
+++ src/main/java/com/jinjjaseoul/domain/user/dto/UserResponseDto.java
@@ -0,0 +1,28 @@
+package com.jinjjaseoul.domain.user.dto;
+
+import com.jinjjaseoul.common.enums.Provider;
+import com.jinjjaseoul.common.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponseDto {
+
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+    private String introduction;
+    private Long iconId;
+    private Role role;
+    private Provider provider;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+}
