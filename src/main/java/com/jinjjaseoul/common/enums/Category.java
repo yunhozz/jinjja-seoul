43d53949cb2228b8b9a0074e561137bf76c6327c
yunhozz <qkrdbsgh1121@naver.com>
2022-12-11 19:08:06 +0900

    fix: 각 카테고리 설명 추가

--- src/main/java/com/jinjjaseoul/common/enums/Category.java
+++ src/main/java/com/jinjjaseoul/common/enums/Category.java
@@ -1,5 +1,25 @@
 package com.jinjjaseoul.common.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Category {
-    FOOD, CAFE, BAR, STORE, WORK, LIFE, PET, CULTURE;
+
+    FOOD("식당"),
+    CAFE("카페"),
+    BAR("주점"),
+    STORE("상점"),
+    WORK("작업"),
+    LIFE("일상"),
+    PET("반려동물"),
+    CULTURE("문화공간"),
+    ETC("기타"),
+
+    ;
+
+    private final String desc;
+
+    Category(String desc) {
+        this.desc = desc;
+    }
 }
