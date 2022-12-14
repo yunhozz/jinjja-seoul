a4a44deb372d46a3531850b0bafdfe7fe1dd6346
yunhozz <qkrdbsgh1121@naver.com>
2022-12-14 19:00:22 +0900

    fix: 쿼리 오류 수정

--- src/main/java/com/jinjjaseoul/domain/user/model/UserRepository.java
+++ src/main/java/com/jinjjaseoul/domain/user/model/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, UserCustomRepository {
 
     Optional<User> findByEmail(String email);
 
