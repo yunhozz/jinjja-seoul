c7cc8d1d6a241951019fc8cdaa78750c6c28850e
yunhozz <qkrdbsgh1121@naver.com>
2022-12-27 16:00:45 +0900

    feat: 유저 활동점수 관련 컬럼 초기화 쿼리 생성

--- src/main/java/com/jinjjaseoul/JinjjaSeoulApplication.java
+++ src/main/java/com/jinjjaseoul/JinjjaSeoulApplication.java
@@ -3,9 +3,11 @@ package com.jinjjaseoul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
+@SpringBootApplication
 public class JinjjaSeoulApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/jinjjaseoul/domain/user/model/repository/UserRepository.java
+++ src/main/java/com/jinjjaseoul/domain/user/model/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.jinjjaseoul.domain.user.model.repository;
 
 import com.jinjjaseoul.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -13,4 +14,8 @@ public interface UserRepository extends JpaRepository<User, Long>, UserCustomRep
 
     @Query("select u from User u join fetch u.icon i where u.id = :id")
     Optional<User> findWithIconById(@Param("id") Long userId);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update User u set u.numOfRecommend = 0, u.numOfComment = 0")
+    void initNumOfActive();
 }
