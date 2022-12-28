54e89403a4de1ae221e7a75e5737f8ce4e65119a
yunhozz <qkrdbsgh1121@naver.com>
2022-12-28 19:29:34 +0900

    rename: 클래스 명 변경

--- src/main/java/com/jinjjaseoul/common/utils/SchedulerTasks.java
+++ src/main/java/com/jinjjaseoul/common/utils/SchedulerTasks.java
@@ -12,12 +12,12 @@ import java.time.format.DateTimeFormatter;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class ScheduledTasks {
+public class SchedulerTasks {
 
     private final UserRepository userRepository;
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("mm:ss:SSS");
 
-    // 매주 오전 6시마다 전체 유저의 활동점수 초기화
+    // 매주 월요일 오전 6시마다 전체 유저의 활동점수 초기화
     @Scheduled(cron = "0 0 6 * * MON", zone = "Asia/Seoul")
     public void initUserRecommendNumbers() {
         log.info("현재 시간 = {}", formatter.format(LocalDateTime.now()));
