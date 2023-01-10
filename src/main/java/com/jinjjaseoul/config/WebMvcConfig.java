a3c0efcc94d2e3c9130eb16ebeaa7c1e52c75621
yunhozz <qkrdbsgh1121@naver.com>
2023-01-10 17:50:35 +0900

    feat: view 레지스트리 등록

--- src/main/java/com/jinjjaseoul/config/WebMvcConfig.java
+++ src/main/java/com/jinjjaseoul/config/WebMvcConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -19,6 +20,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .maxAge(MAX_AGE_SECS);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/login-page").setViewName("login-page");
+        registry.addViewController("/sign-up").setViewName("join");
+        registry.addViewController("/sign-in").setViewName("login");
     }
 
     @Bean
