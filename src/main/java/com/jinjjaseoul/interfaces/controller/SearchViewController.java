7fd71cda54e681ee8f37e6d0b37b38ca7ad6a07f
yunhozz <qkrdbsgh1121@naver.com>
2023-01-14 15:17:33 +0900

    feat: 지도 검색 관련 파일 추가

--- src/main/java/com/jinjjaseoul/interfaces/controller/SearchViewController.java
+++ src/main/java/com/jinjjaseoul/interfaces/controller/SearchViewController.java
@@ -1,24 +1,25 @@
 package com.jinjjaseoul.interfaces.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Slf4j
 @Controller
 @RequestMapping("/map-finder")
 @RequiredArgsConstructor
 public class SearchViewController {
 
     @GetMapping
-    public String redirectPageWithKeyword(@RequestParam String keyword, RedirectAttributes redirectAttributes) {
-        redirectAttributes.addFlashAttribute("keyword", keyword);
+    public String redirectPageWithKeyword(@RequestParam String keyword, Model model) {
+        model.addAttribute("keyword", keyword);
         return "search-redirect";
     }
 
