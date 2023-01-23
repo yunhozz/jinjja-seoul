b8f9d9a42085434db52bfa4923e797ed9c8e82ae
yunhozz <qkrdbsgh1121@naver.com>
2023-01-23 15:56:21 +0900

    fix: 키워드 검색 리다이렉트 관련 로직 수정

--- src/main/java/com/jinjjaseoul/interfaces/controller/SearchViewController.java
+++ src/main/java/com/jinjjaseoul/interfaces/controller/SearchViewController.java
@@ -3,13 +3,11 @@ package com.jinjjaseoul.interfaces.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
 @Controller
@@ -17,16 +15,14 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class SearchViewController {
 
-    @GetMapping
-    public String redirectPageWithKeyword(@RequestParam(required = false) String keyword, Model model) {
-        model.addAttribute("keyword", keyword);
-        return "search-redirect";
+    @PostMapping
+    public String redirectToSearchPage(@RequestParam(required = false) String keyword, RedirectAttributes attr) {
+        attr.addFlashAttribute("keyword", keyword);
+        return "redirect:/map-finder";
     }
 
-    @PostMapping
-    public String searchPage(HttpServletRequest request, Model model) {
-        String keyword = request.getParameter("keyword");
-        model.addAttribute("keyword", keyword);
+    @GetMapping
+    public String searchPage() {
         return "map-list";
     }
 }
